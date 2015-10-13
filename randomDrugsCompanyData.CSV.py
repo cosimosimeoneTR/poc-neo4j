f7a56ec5a7379d943e9683af8f88fdd5829063cb
Debian <admin@ip-172-31-18-192.ec2.internal>
2015-10-13 16:41:56 +0000

    Changed companydrugs.csv spooling

--- randomDrugsCompanyData.CSV.py
+++ randomDrugsCompanyData.CSV.py
@@ -8,10 +8,10 @@ import pickle
 
 v_attribNum = 10
 
-v_companyNum = 10
-v_drugNum = 250
-v_sparseRnd1 = 10
-v_sparseRnd2 = 30
+v_companyNum = 1000
+v_drugNum = 250000
+v_sparseRnd1 = 100
+v_sparseRnd2 = 300
 v_trialNum = 100
 
 
@@ -23,7 +23,7 @@ def printAttribs( p_attribNum ):
 
 
 fileOut = open('DONTBACKUP/drugs.csv', 'wb')
-fileOut.write('id:ID(Drug),:LABEL'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write('drugId:ID(Drug),:LABEL'+printAttribs ( v_attribNum ) +'\n')
 for drugId in range(0,v_drugNum+1):
   fileOut.write(str(drugId)+',Drg_'+str(drugId)+printAttribs ( v_attribNum ) +'\n')
 fileOut.close()
@@ -31,15 +31,15 @@ fileOut.close()
 
 
 fileOut = open('DONTBACKUP/companies.csv', 'wb')
-fileOut.write('id:ID(Company),:LABEL'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write('companyId:ID(Company),:LABEL'+printAttribs ( v_attribNum ) +'\n')
 for companyId in range(0,v_companyNum+1):
   fileOut.write(str(companyId)+',Cmp_'+str(companyId)+printAttribs ( v_attribNum ) +'\n')
 fileOut.close()
 
 fileOut = open('DONTBACKUP/companiesdrugs.csv', 'wb')
-fileOut.write(':START_ID(Company),:END_ID(Drug),:TYPE'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write(':START_ID(Drug),:END_ID(Company),:TYPE'+printAttribs ( 5 ) +'\n')
 for drugId in range(0,v_drugNum):
-  fileOut.write(str(companyId)+','+str(drugId%v_companyNum)+',PRODUCES'+printAttribs ( v_attribNum ) +'\n')
+  fileOut.write(str(random.randint(1, v_drugNum ))+','+str(drugId%v_companyNum)+',PRODUCES'+printAttribs ( 5 ) +'\n')
 fileOut.close()
 
 
