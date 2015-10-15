ea6510e0d5f42db9b17a103a0c1a16c7e7b9bb05
cosimosimeoneTR <cosimo.simeone@thomsonreuters.com>
2015-10-15 14:36:31 +0200

    Random attributes to chech indexes, plus should be faster

--- randomDrugsCompanyData.CSV.py
+++ randomDrugsCompanyData.CSV.py
@@ -5,6 +5,7 @@ import os
 import random
 import json
 import pickle
+import string
 
 v_attribNum = 10
 
@@ -15,49 +16,76 @@ v_sparseRnd2 = 200
 v_trialNum = 1000000
 
 
-def printAttribs( p_attribNum ):
+def printAttribsHead( p_attribNum ):
   retVal = ""
   for attribId in range(0,v_attribNum):
     retVal = retVal+',attr'+str(attribId)
   return retVal
 
+def printAttribsCnt ( p_attribNum ):
+  retVal = ""
+  for attribId in range(0,v_attribNum):
+    retVal = retVal+',attr'+str(attribId)+str(''.join(random.choice(string.lowercase) for i in range(10)) )
+  return retVal
 
+myString=""
 fileOut = open('DONTBACKUP/drugs.csv', 'wb')
-fileOut.write('id:ID(Drug),:LABEL,drugName'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write('id:ID(Drug),:LABEL,drugName'+printAttribsHead ( v_attribNum ) +'\n')
 for drugId in range(0,v_drugNum-1):
-  fileOut.write(str(drugId)+',Drug,Drg_'+str(drugId)+printAttribs ( v_attribNum ) +'\n')
+  myString=myString+(str(drugId)+',Drug,Drg_'+str(drugId)+printAttribsCnt ( v_attribNum ) +'\n')
+  if drugId % 100:
+    fileOut.write(myString)
+    myString=""
+fileOut.write(myString)
 fileOut.close()
 
 
-
+myString=""
 fileOut = open('DONTBACKUP/companies.csv', 'wb')
-fileOut.write('id:ID(Company),:LABEL,companyName'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write('id:ID(Company),:LABEL,companyName'+printAttribsHead ( v_attribNum ) +'\n')
 for companyId in range(0,v_companyNum-1):
-  fileOut.write(str(companyId)+',Company,Cmp_'+str(companyId)+printAttribs ( v_attribNum ) +'\n')
+  myString=myString+(str(companyId)+',Company,Cmp_'+str(companyId)+printAttribsCnt ( v_attribNum ) +'\n')
+  if companyId % 100:
+    fileOut.write(myString)
+    myString=""
+fileOut.write(myString)
 fileOut.close()
 
+
+myString=""
 fileOut = open('DONTBACKUP/companiesdrugs.csv', 'wb')
-fileOut.write(':START_ID(Company),:END_ID(Drug),:TYPE'+printAttribs ( 5 ) +'\n')
+fileOut.write(':START_ID(Company),:END_ID(Drug),:TYPE'+printAttribsHead ( 5 ) +'\n')
 for drugId in range(0,v_drugNum-1):
-  fileOut.write(str(drugId%(v_companyNum-1))+','+str(drugId)+',PRODUCES'+printAttribs ( 5 ) +'\n')
+  myString=myString+(str(drugId%(v_companyNum-1))+','+str(drugId)+',PRODUCES'+printAttribsCnt ( 5 ) +'\n')
+  if drugId % 100:
+    fileOut.write(myString)
+    myString=""
+fileOut.write(myString)
 fileOut.close()
 
 
-
+myString=""
 fileOut = open('DONTBACKUP/trials.csv', 'wb')
-fileOut.write('id:ID(Trial),:LABEL,trialName'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write('id:ID(Trial),:LABEL,trialName'+printAttribsHead ( v_attribNum ) +'\n')
 for trialId in range(0,v_trialNum-1):
-  fileOut.write(str(trialId)+',trial,trial_'+str(drugId)+printAttribs ( v_attribNum ) +'\n')
+  myString=myString+(str(trialId)+',trial,trial_'+str(drugId)+printAttribsCnt ( v_attribNum ) +'\n')
+  if trialId % 100:
+    fileOut.write(myString)
+    myString=""
+fileOut.write(myString)
 fileOut.close()
 
 
+myString=""
 fileOut = open('DONTBACKUP/trialsdrugs.csv', 'wb')
-fileOut.write(':START_ID(Trial),:END_ID(Drug),:TYPE,code'+printAttribs ( v_attribNum ) +'\n')
+fileOut.write(':START_ID(Trial),:END_ID(Drug),:TYPE,code'+printAttribsHead ( v_attribNum ) +'\n')
 for trialId in range(0,v_trialNum-1):
-  myString=""
-  myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd1))+',RELATED_TO,1'+printAttribs ( v_attribNum ) +'\n'
-  myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd2))+',RELATED_TO,2'+printAttribs ( v_attribNum ) +'\n'
-  myString=myString+str(trialId)+','+str(random.randint(1, v_drugNum   ))+',RELATED_TO,3'+printAttribs ( v_attribNum ) +'\n'
-  fileOut.write(myString)
+  myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd1))+',RELATED_TO,1'+printAttribsCnt ( v_attribNum ) +'\n'
+  myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd2))+',RELATED_TO,2'+printAttribsCnt ( v_attribNum ) +'\n'
+  myString=myString+str(trialId)+','+str(random.randint(1, v_drugNum   ))+',RELATED_TO,3'+printAttribsCnt ( v_attribNum ) +'\n'
+  if trialId % 100:
+    fileOut.write(myString)
+    myString=""
+fileOut.write(myString)
 fileOut.close()
 
