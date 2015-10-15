5edaa32f9a36ba278a57c1461490f8419843aa25
Cosimo Simeone <cosimo.simeone@thomsonreuters.com>
2015-10-15 14:46:58 +0200

    added commit var

--- randomDrugsCompanyData.CSV.py
+++ randomDrugsCompanyData.CSV.py
@@ -8,6 +8,7 @@ import pickle
 import string
 
 v_attribNum = 10
+v_writeCommit = 1000
 
 v_companyNum = 1000
 v_drugNum = 3000000
@@ -33,7 +34,7 @@ fileOut = open('DONTBACKUP/drugs.csv', 'wb')
 fileOut.write('id:ID(Drug),:LABEL,drugName'+printAttribsHead ( v_attribNum ) +'\n')
 for drugId in range(0,v_drugNum-1):
   myString=myString+(str(drugId)+',Drug,Drg_'+str(drugId)+printAttribsCnt ( v_attribNum ) +'\n')
-  if drugId % 100:
+  if drugId % v_writeCommit:
     fileOut.write(myString)
     myString=""
 fileOut.write(myString)
@@ -45,7 +46,7 @@ fileOut = open('DONTBACKUP/companies.csv', 'wb')
 fileOut.write('id:ID(Company),:LABEL,companyName'+printAttribsHead ( v_attribNum ) +'\n')
 for companyId in range(0,v_companyNum-1):
   myString=myString+(str(companyId)+',Company,Cmp_'+str(companyId)+printAttribsCnt ( v_attribNum ) +'\n')
-  if companyId % 100:
+  if companyId % v_writeCommit:
     fileOut.write(myString)
     myString=""
 fileOut.write(myString)
@@ -57,7 +58,7 @@ fileOut = open('DONTBACKUP/companiesdrugs.csv', 'wb')
 fileOut.write(':START_ID(Company),:END_ID(Drug),:TYPE'+printAttribsHead ( 5 ) +'\n')
 for drugId in range(0,v_drugNum-1):
   myString=myString+(str(drugId%(v_companyNum-1))+','+str(drugId)+',PRODUCES'+printAttribsCnt ( 5 ) +'\n')
-  if drugId % 100:
+  if drugId % v_writeCommit:
     fileOut.write(myString)
     myString=""
 fileOut.write(myString)
@@ -69,7 +70,7 @@ fileOut = open('DONTBACKUP/trials.csv', 'wb')
 fileOut.write('id:ID(Trial),:LABEL,trialName'+printAttribsHead ( v_attribNum ) +'\n')
 for trialId in range(0,v_trialNum-1):
   myString=myString+(str(trialId)+',trial,trial_'+str(drugId)+printAttribsCnt ( v_attribNum ) +'\n')
-  if trialId % 100:
+  if trialId % v_writeCommit:
     fileOut.write(myString)
     myString=""
 fileOut.write(myString)
@@ -83,9 +84,8 @@ for trialId in range(0,v_trialNum-1):
   myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd1))+',RELATED_TO,1'+printAttribsCnt ( v_attribNum ) +'\n'
   myString=myString+str(trialId)+','+str(random.randint(1, v_sparseRnd2))+',RELATED_TO,2'+printAttribsCnt ( v_attribNum ) +'\n'
   myString=myString+str(trialId)+','+str(random.randint(1, v_drugNum   ))+',RELATED_TO,3'+printAttribsCnt ( v_attribNum ) +'\n'
-  if trialId % 100:
+  if trialId % v_writeCommit:
     fileOut.write(myString)
     myString=""
 fileOut.write(myString)
 fileOut.close()
-
