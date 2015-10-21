6ce5a50e9b817bddc3ae598f84ad1fd32bead202
Debian <admin@ip-172-31-26-164.ec2.internal>
2015-10-21 08:10:34 +0000

    Changed ls sorting, now alphabetical

--- __watch.sh
+++ __watch.sh
@@ -1,1 +1,1 @@
-watch -n 3 'ps -ef | grep python|grep -v watch|grep -v grep;echo;ls -lrtk DONTBACKUP/*csv* ;echo; df -h ; echo; free -m '
+watch -n 3 'ps -ef | grep python|grep -v watch|grep -v grep;echo;ls -lk DONTBACKUP/*csv* ;echo; df -h ; echo; free -m '
