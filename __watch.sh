fe29929c5cb30cc12dea0369d3e8dd1d70034a8a
Debian <admin@ip-172-31-26-164.ec2.internal>
2015-10-21 07:54:15 +0000

    Some better and enchanged visualization changes

--- __watch.sh
+++ __watch.sh
@@ -1,1 +1,1 @@
-watch -n 3 'ls -lrth DONTBACKUP/*csv* ; echo;echo;echo; df -h ; echo;echo; free -m '
+watch -n 3 'ps -ef | grep python|grep -v watch|grep -v grep;echo;ls -lrtk DONTBACKUP/*csv* ;echo; df -h ; echo; free -m '
