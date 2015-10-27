08b01540c59f37c2e229fdce5528b80a092b9a71
Debian <admin@ip-172-31-20-12.ec2.internal>
2015-10-27 13:58:05 +0000

    Added _LOAD_ALL.parallel.sh : Just import, no CSVs creation

---
+++ _LOAD_ALL.parallel.sh
@@ -0,0 +1,12 @@
+#####sudo rm -rf DONTBACKUP/load-test/db/* && \
+#####./randomData.parameter_ALL.sh >/dev/null 2>&1 && \
+#####for file in `ls DONTBACKUP/*csv.gz`;do echo $file `zcat $file | wc -l`; done;echo ;echo  && \
+
+./neo4jImport.biggerCSV.sh && \
+echo ;echo "Restarting neo4jserver..." && \
+sudo chown -R neo4j:nogroup DONTBACKUP/load-test/ && \
+sudo service neo4j restart && \
+echo "DONE!"
+
+
+
