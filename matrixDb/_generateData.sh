7f7bc4008408d396b5620dce3246408f0eb148da
Debian <admin@ip-172-31-53-19.ec2.internal>
2015-10-28 14:20:54 +0000

    Working on...

--- matrixDb/_generateData.sh
+++ matrixDb/_generateData.sh
@@ -11,7 +11,7 @@ nohup ./randomData.entities.matrixDb.py  8        1000000   /mnt/dataDisk/matrix
 nohup ./randomData.entities.matrixDb.py  9        1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 nohup ./randomData.entities.matrixDb.py 10        1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
   
-nohup ./randomData.relations.matrixDb.py  1  10   1000000   /mnt/dataDisk/matrix/   > `basename $0`.log  2>&1
+nohup ./randomData.relations.matrixDb.py  1  10   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 nohup ./randomData.relations.matrixDb.py  2   1   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 nohup ./randomData.relations.matrixDb.py  3   2   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 nohup ./randomData.relations.matrixDb.py  4   3   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
@@ -23,4 +23,6 @@ nohup ./randomData.relations.matrixDb.py  9   8   1000000   /mnt/dataDisk/matrix
 nohup ./randomData.relations.matrixDb.py 10   9   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 
 
+nohup ./randomData.relations.matrixDb.py  2   4   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
+nohup ./randomData.relations.matrixDb.py  4   2   1000000   /mnt/dataDisk/matrix/  >> `basename $0`.log  2>&1
 
