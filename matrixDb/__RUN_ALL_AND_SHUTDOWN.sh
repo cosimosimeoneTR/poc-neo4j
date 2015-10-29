feeb6b2c55f78e5a340f287468c3e79af21c23db
Debian <admin@ip-172-31-63-213.ec2.internal>
2015-10-29 12:34:09 +0000

    The idea is run this script on different instances/instances type and see performance. Very automatic data creation, load and indexing..

--- matrixDb/__RUN_ALL_AND_SHUTDOWN.sh
+++ matrixDb/__RUN_ALL_AND_SHUTDOWN.sh
@@ -1,8 +1,31 @@
-touch zzz_STARTED_AT.log
+set -x
 
-./_generateData.sh && ./_importData.sh
+touch zzz_STARTED_AT.log
+cd
+cd poc-neo4j-import/
+git pull origin master
+cd
+cd poc-neo4j-stressTest/
+git pull origin master
+cd
+cd poc-neo4j-import/matrixDb/
+
+date
+curl -s http://instance-data/latest/meta-data/ami-id;echo;curl -s http://instance-data/latest/meta-data/instance-type;echo; curl -s http://instance-data/latest/meta-data/instance-id
+
+echo -n "******************************************************* " && date
+./_generateData.sh 
+echo -n "******************************************************* " && date
+./_importData.sh
+echo -n "******************************************************* " && date
+
+
+cd
+cd poc-neo4j-stressTest/
+./recreateIndexes.sh
+echo -n "******************************************************* " && date
 
 touch zzz_ENDED_AT.log
-
+date
 sudo shutdown -h now
 
