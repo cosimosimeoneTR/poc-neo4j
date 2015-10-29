16bfaacb2638442c607f6290ef24d3193b9c2b25
Debian <admin@ip-172-31-53-19.ec2.internal>
2015-10-29 08:02:20 +0000

    Added log files to see when execution starts/ends

--- matrixDb/__RUN_ALL.sh
+++ matrixDb/__RUN_ALL.sh
@@ -1,1 +1,5 @@
+touch STARTED_AT.log
+
 ./_generateData.sh && ./_importData.sh
+
+touch ENDED_AT.log
--- matrixDb/__RUN_ALL_AND_SHUTDOWN.sh
+++ matrixDb/__RUN_ALL_AND_SHUTDOWN.sh
@@ -1,4 +1,5 @@
 touch STARTED_AT.log
+
 ./_generateData.sh && ./_importData.sh
 
 touch ENDED_AT.log
