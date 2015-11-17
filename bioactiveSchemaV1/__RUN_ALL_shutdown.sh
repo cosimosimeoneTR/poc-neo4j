e7b61cd6b7dfc28fed2ee3f9279a5bd45c022ce3
Debian <admin@ip-172-31-6-107.eu-west-1.compute.internal>
2015-11-17 10:40:01 +0000

    Added parallel data creation and shutdown

---
+++ bioactiveSchemaV1/__RUN_ALL_shutdown.sh
@@ -0,0 +1,10 @@
+curl -s http://instance-data/latest/meta-data/ami-id
+echo
+curl -s http://instance-data/latest/meta-data/instance-type
+echo
+curl -s http://instance-data/latest/meta-data/instance-id
+echo
+
+./_generateData.sh  /mnt/dataDisk/bioactiveSchemaV1/    2000000    0    50000                        &&
+./_importData.sh    /mnt/dataDisk/bioactiveSchemaV1/    /mnt/databaseDisk/bioactiveSchemaV1/         &&
+./constraintCreate.sh
