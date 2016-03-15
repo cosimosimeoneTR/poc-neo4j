676497261d377311e0ba63576749c9ba9ebb75f5
Debian <admin@ip-172-31-14-112.eu-west-1.compute.internal>
2016-03-15 11:38:55 +0000

    Back to more random data

--- relCntTest/randomData.entities.matrixDb.py
+++ relCntTest/randomData.entities.matrixDb.py
@@ -24,8 +24,8 @@ def printAttribsCnt ( p_attribNum ):
   retVal = ""
   for attribId in range(0,v_attribNum):
     #retVal = retVal+',attr'+str(attribId)+str(''.join(random.choice(string.lowercase) for i in range(10)) )
-    #retVal = retVal+',attr'+str(attribId)+'_'+str( random.randint(0, 9999) )
-    retVal = retVal+',attr'+str(attribId)+'_blaBlaBla'
+    retVal = retVal+',attr'+str(attribId)+'_'+str( random.randint(0, 9999) )
+    #retVal = retVal+',attr'+str(attribId)+'_blaBlaBla'
   return retVal
 
 fixedAttribs=printAttribsCnt ( v_attribNum )
@@ -36,8 +36,8 @@ fileOut.write('id:ID('+str(v_entityId)+'),:LABEL,entityName'+printAttribsHead ( 
 for loopId in range(1,v_rowNums+1):
 
 
-  #myString=myString+(str(loopId)+','+str(v_entityId)+','+str(v_entityId)+'_'+str(loopId)+printAttribsCnt ( v_attribNum ) +'\n')
-  myString=myString+(str(loopId)+','+str(v_entityId)+','+str(v_entityId)+'_'+str(loopId)+ fixedAttribs +'\n')
+  myString=myString+(str(loopId)+','+str(v_entityId)+','+str(v_entityId)+'_'+str(loopId)+printAttribsCnt ( v_attribNum ) +'\n')
+  #myString=myString+(str(loopId)+','+str(v_entityId)+','+str(v_entityId)+'_'+str(loopId)+ fixedAttribs +'\n')
   if loopId % v_writeCommit:
     fileOut.write(myString)
     myString=""
--- relCntTest/randomData.relations.matrixDb.py
+++ relCntTest/randomData.relations.matrixDb.py
@@ -24,8 +24,8 @@ def printAttribsCnt ( p_attribNum ):
   retVal = ""
   for attribId in range(0,v_attribNum):
     #retVal = retVal+',attr'+str(attribId)+str(''.join(random.choice(string.lowercase) for i in range(10)) )
-    #retVal = retVal+',attr'+str(attribId)+'_'+str( random.randint(0, 999999) )
-    retVal = retVal+',attr'+str(attribId)+'_blaBlaBla'
+    retVal = retVal+',attr'+str(attribId)+'_'+str( random.randint(0, 999999) )
+    #retVal = retVal+',attr'+str(attribId)+'_blaBlaBla'
   return retVal
 
 fixedAttribs=printAttribsCnt ( v_attribNum )
@@ -36,8 +36,8 @@ fileOut.write(':START_ID('+str(v_startEntityId)+'),:END_ID('+str(v_endEntityId)+
 for loopId in range(1,v_rowNums):
   if v_relationTypeNum !=0: relationType='Relation'+str( (loopId%v_relationTypeNum)+1 )
   else: relationType='Relation'
-  #myString=myString+(str(loopId)+','+str( random.randint(1 , ((loopId + 10) % (v_rowNums-5) )+1  ) ) +','+str(v_startEntityId)+'2'+str(v_endEntityId)+','+relationType +printAttribsCnt ( v_attribNum ) +'\n')
-  myString=myString+(str(loopId)+','+str( random.randint(1 , v_rowNums-5) ) +','+str(v_startEntityId)+'2'+str(v_endEntityId)+','+relationType +fixedAttribs +'\n')
+  myString=myString+(str(loopId)+','+str( random.randint(1 , ((loopId + 10) % (v_rowNums-5) )+1  ) ) +','+str(v_startEntityId)+'2'+str(v_endEntityId)+','+relationType +printAttribsCnt ( v_attribNum ) +'\n')
+  #myString=myString+(str(loopId)+','+str( random.randint(1 , v_rowNums-5) ) +','+str(v_startEntityId)+'2'+str(v_endEntityId)+','+relationType +fixedAttribs +'\n')
   if loopId % v_writeCommit:
     fileOut.write(myString)
     myString=""
